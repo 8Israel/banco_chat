0c270113756a53229ffa83414721b2fa5502b415
FernandoYZD <168001846+FernandoYZD@users.noreply.github.com>
2026-09-12 15:48:04 -0600

    feat: generate cards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { CardsModule } from './modules/cards/cards.module.js';
 
 @Module({
-  imports: [],
+  imports: [CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })

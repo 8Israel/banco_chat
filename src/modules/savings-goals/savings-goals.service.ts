498432edfca646f876c238f1b3e2864ba098fb29
evatech399 <evatech399@gmail.com>
2026-09-12 19:23:21 -0600

    primera chat

--- src/modules/savings-goals/savings-goals.service.ts
+++ src/modules/savings-goals/savings-goals.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSavingsGoalDto } from './dto/create-savings-goal.dto.js';
 import { UpdateSavingsGoalDto } from './dto/update-savings-goal.dto.js';
+import { PrismaService } from '../../infrastructure/prisma/prisma.service.js';
 
 @Injectable()
 export class SavingsGoalsService {
-  create(createSavingsGoalDto: CreateSavingsGoalDto) {
-    return 'This action adds a new savingsGoal';
+
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(req: CreateSavingsGoalDto) {
+
+    const newGoal = await this.prisma.savingsGoal.create({
+      data: {
+        accountId: req.accountId,
+        name: req.name,
+        targetAmount:req.targetAmount,
+        actualAmount:0
+      }
+    })
+    return newGoal
   }
 
-  findAll() {
-    return `This action returns all savingsGoals`;
+  async findAll() {
+    return await this.prisma.savingsGoal.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} savingsGoal`;
+  async findOne(id: number) {
+    const goal = await this.prisma.savingsGoal.findFirst({where: {id}});
+
+    if (!goal) {
+      throw new NotFoundException('Objetivo no existente');
+    }
+    
+    return goal;
   }
 
-  update(id: number, updateSavingsGoalDto: UpdateSavingsGoalDto) {
-    return `This action updates a #${id} savingsGoal`;
+  async update(id: number, userId:number, req: UpdateSavingsGoalDto) {
+    const goal = await this.prisma.savingsGoal.findFirst({where: {id}});
+
+    if (!goal) {
+      throw new NotFoundException('Objetivo no existente');
+    }
+    const card = await this.prisma.account.findFirst({where: {id:goal.accountId, userId}})
+    if (!card) {
+      throw new NotFoundException('Objetivo no perteneciente al usuario');
+    }
+
+    const goalUpdate = await this.prisma.savingsGoal.update({
+      where: { id },
+      data: {
+        name: req.name ?? goal.name,
+        targetAmount:req.targetAmount ?? goal.targetAmount,
+        actualAmount:req.actualAmount ?? goal.actualAmount
+      }
+    })
+    return goalUpdate
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} savingsGoal`;
+  async remove(id: number) {
+    const goal = await this.prisma.savingsGoal.findFirst({where: {id}});
+
+    if (!goal) {
+      throw new NotFoundException('Objetivo no existente');
+    }
+
+    return await this.prisma.savingsGoal.delete({
+      where: { id }
+    })
   }
 }
